6c627b7f32124b10862c8965ceb0e806286c5edd
georgemaina <george@chak.or.ke>
2021-10-07 09:40:18 +0300

    cashbook payments updates

---
+++ data/conn.php
@@ -0,0 +1,10 @@
+<?php
+//Define your host here.
+$HostName = "localhost";
+//Define your MySQL Database Name here.
+$DatabaseName = "careNyanchwa";
+//Define your Database UserName here.
+$HostUser = "george";
+//Define your Database Password here.
+$HostPass = "&355ewxxKE";
+?>
